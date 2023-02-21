8a176f3a04334dc02e5a7c3d9f142817b312e48a
CarlosAndres <solrak41@gmail.com>
2023-02-20 22:48:47 -0500

    Add player Factory and gameboard Module

---
+++ app.js
@@ -0,0 +1,11 @@
+// Player Factory Function:
+
+const player = (name, marker) => {
+    return { name, marker };
+};
+
+// Gameboard Module
+
+const gameBoard = (function () {
+    gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+})();
