25ebe14b2b4ae28fb69643847fe6b913bd588bb5
CarlosAndres <solrak41@gmail.com>
2023-02-20 23:29:45 -0500

    Set up a JavaScript function that will render the contents of the gameboard array to the webpage

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const player = (name, marker) => {
 
 // Gameboard Module
 
-const gameBoard = (function () {
-    gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+const newGameBoard = (() => {
+    const gameboardDiv = document.querySelector('.gameboard');
+    const array = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+    const renderArray = () => {
+        array.forEach((element) => {
+            gameboardDiv.append(document.createElement('div'));
+        });
+    };
+    return {
+        renderArray,
+    };
 })();
+
+console.log(newGameBoard.renderArray());
